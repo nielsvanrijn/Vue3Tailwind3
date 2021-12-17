59705379ff9010918053b13ee0ce95518f8e6ae2
Niels van Rijn <nielsvanrijn@7lab.nl>
2021-12-17 10:42:10 +0100

    Added forms

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <input type="checkbox" class="rounded text-pink-500" />
     <p class="text-blue-600">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
